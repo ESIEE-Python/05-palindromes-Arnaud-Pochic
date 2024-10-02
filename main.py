668b779d9128b7b86e371cb5dbed978a609a309d
Arnaud Pochic <121885943+Arnaud-Pochic@users.noreply.github.com>
2024-10-02 08:40:20 +0000

    exo palindromes

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 #### Fonction secondaire
-
+"""
+Auteur : Arnaud POCHIC
+"""
 
 def ispalindrome(p):
+    """
+    Retourne True si la chaîne de caractères p est un palindrome,
+    et retourne Faux dans le cas contraire
 
-    # votre code ici
-    
-    return False
+    Args:
+        p: chaîne de caractères
+
+    Returns:
+        True or False
+    """
+    p=p.lower()
+    dic =  str.maketrans("âäàéêëèôöçûüùîïÿ", "aaaeeeeoocuuuiiy", " '&~#{(-|_\\^@)°])}=+-$£!:;?.,")
+    p = p.translate(dic)
+    if p != p[::-1]:
+        return False
+
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    programme principal qui appelle et teste la fonction ispalindrome()
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
